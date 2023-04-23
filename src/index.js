ef760668065a412b256e31c4748df4ff237ef584
Ibrahim Kolabalogun <115741084+bolajeee@users.noreply.github.com>
2023-04-23 18:30:59 +0100

    it

--- src/index.js
+++
@@ -1,6 +0,0 @@
-let burger = document.getElementById('burger')
-let navItems = document.getElementsByClassName('nav-items')
-
-burger.addEventListener('onclick',function(){
-    navItems.style.display = "block";
-})
